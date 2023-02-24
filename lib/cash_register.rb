2b551ba6ef638cb61e443008b19b68d4fdc6fc6a
Bernard <bernard.koech@student.moringaschool.com>
2023-02-24 11:40:59 +0300

    commit tree f596a079d3b8f51f53997c387539ba089bf63e9d

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+require 'pry'
+class CashRegister
+  attr_accessor :discount, :total
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+  end
 
+  def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+  end
+
+  def apply_discount
+    case
+    when self.discount
+      discount = @discount/100.0 * self.total
+      self.total -= discount
+      "After the discount, the total comes to $#{self.total.round}."
+    end
+
+  end
+end
+binding.pry
